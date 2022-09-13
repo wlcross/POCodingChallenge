e7fd3e48cac688f15e51d363bd886a8f7729b726
wlcross <william589@revature.net>
2022-09-13 14:03:53 -0400

    Reorder

--- src/september12/ReorderDigits.java
+++ src/september12/ReorderDigits.java
@@ -5,7 +5,42 @@ public class ReorderDigits {
 	
 	public static int[] reorderDigits(int[] arr, String order) {
 		
-		return null;
+		for (int i = 0; i < arr.length; i ++) {
+			char[] init = ("" + arr[i]).toCharArray();
+			char[] sorted = new char[init.length];
+			
+			if (order.equals("desc")) {
+				for (int j = 0; j < sorted.length; j ++) {
+					int highest = 0;
+					for (int k = 0; k < init.length; k ++) {
+						if(init[k] > init[highest]) {
+							highest = k;
+						}
+					}
+					sorted[j] = init[highest];
+					init[highest] = Character.MIN_VALUE;
+				}
+				
+			} else if (order.equals("asc")) {
+				for (int j = 0; j < sorted.length; j ++) {
+					int lowest = 0;
+					for (int k = 0; k < init.length; k ++) {
+						if(init[k] < init[lowest]) {
+							lowest = k;
+						}
+					}
+					sorted[j] = init[lowest];
+					init[lowest] = Character.MAX_VALUE;
+				}
+			} else {
+				return null;
+			}
+			
+			arr[i] = Integer.parseInt(new String(sorted));
+			
+		}
+		
+		return arr;
 	}
 }
 
--- src/september12/ReorderTests.java
+++ src/september12/ReorderTests.java
@@ -9,41 +9,41 @@ class ReorderTests {
 	void example1() {
 		int[] example = {515, 341, 98, 44, 211};
 		int[] actual = {155, 134, 89, 44, 112};
-		assertEquals(ReorderDigits.reorderDigits(example, "asc"), actual);
+		assertArrayEquals(ReorderDigits.reorderDigits(example, "asc"), actual);
 	}
 	
 	@Test
 	void example2() {
 		int[] example = {515, 341, 98, 44, 211};
-		int[] actual = {551, 431, 89, 44, 112};
-		assertEquals(ReorderDigits.reorderDigits(example, "desc"), actual);
+		int[] actual = {551, 431, 98, 44, 211};
+		assertArrayEquals(ReorderDigits.reorderDigits(example, "desc"), actual);
 	}
 	
 	@Test
 	void example3() {
 		int[] example = {63251, 78221};
 		int[] actual = {12356, 12278};
-		assertEquals(ReorderDigits.reorderDigits(example, "asc"), actual);
+		assertArrayEquals(ReorderDigits.reorderDigits(example, "asc"), actual);
 	}
 	
 	@Test
 	void example4() {
 		int[] example = {63251, 78221};
 		int[] actual = {65321, 87221};
-		assertEquals(ReorderDigits.reorderDigits(example, "desc"), actual);
+		assertArrayEquals(ReorderDigits.reorderDigits(example, "desc"), actual);
 	}
 	
 	@Test
 	void example5() {
 		int[] example = {1, 2, 3, 4};
 		int[] actual = {1, 2, 3, 4};
-		assertEquals(ReorderDigits.reorderDigits(example, "asc"), actual);
+		assertArrayEquals(ReorderDigits.reorderDigits(example, "asc"), actual);
 	}
 	
 	@Test
 	void example6() {
 		int[] example = {1, 2, 3, 4};
 		int[] actual = {1, 2, 3, 4};
-		assertEquals(ReorderDigits.reorderDigits(example, "desc"), actual);
+		assertArrayEquals(ReorderDigits.reorderDigits(example, "desc"), actual);
 	}
 }
