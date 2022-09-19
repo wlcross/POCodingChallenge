19c30d7bb5da356fbe08bc63dc4b809b93e16f8e
wlcross <william589@revature.net>
2022-09-19 15:35:26 -0400

    Implement sumdigits

--- src/september19/SumDigits.java
+++ src/september19/SumDigits.java
@@ -5,6 +5,17 @@ public class SumDigits {
 	public static int[] sum(int T, String[] S) {
 		int[] result = new int[T];
 		
+		for (int i = 0; i < T; i ++) {
+			char[] charray = S[i].toCharArray();
+			for (int j = 0; j < charray.length; j ++) {
+				if (charray[j] >= '0' && charray[j] <= '9') {
+					//0 character is I believe 48 but this 
+					//protects against that changing for whatever reason
+					result[i] += charray[j] - '0';
+				}
+			}
+		}
+		
 		return result;
 	}
 }
