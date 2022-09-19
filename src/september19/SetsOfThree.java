2bb3a32bc09fb935beb1cf9fcc799231a7699f02
wlcross <william589@revature.net>
2022-09-19 15:28:42 -0400

    First challenge implemented

--- src/september19/SetsOfThree.java
+++ src/september19/SetsOfThree.java
@@ -3,7 +3,19 @@ package september19;
 public class SetsOfThree {
 
 	public static int[] secondMax(int N, int[][] trip) {
+		int[] result = new int[N];
 		
-		return null;
+		for (int i = 0; i < N; i ++) {
+			if (trip[i][0] > trip[i][1] && trip[i][0] > trip[i][2]) {
+				result[i] = Integer.max(trip[i][1], trip[i][2]);
+			} else if (trip[i][0] < trip[i][1] && trip[i][0] < trip[i][2]) {
+				result[i] = Integer.min(trip[i][1], trip[i][2]);
+			} else {
+				result[i] = trip[i][0];
+			}
+		}
+		
+		
+		return result;
 	}
 }
